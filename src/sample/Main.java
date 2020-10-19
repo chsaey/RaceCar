3479b18ab771b0a0d7afa7204b5e0e015bfee0a1
Charles Saeyang <chsaeydev@gmail.com>
2020-10-19 15:15:24 -0700

    Preliminary UI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setScene(new Scene(root, 1000, 720
+
+        ));
         primaryStage.show();
     }
 
