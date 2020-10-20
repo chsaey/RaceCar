e60d70c6db3fcc4301e5e1ebafa1d520bbd97f81
Charles Saeyang <chsaeydev@gmail.com>
2020-10-20 13:01:52 -0700

    Added Pause and resume button
    Added Pause + Resume. Methods still need work.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,33 +1,34 @@
 package sample;
+
 import javafx.concurrent.Service;
 import javafx.fxml.FXML;
 import java.security.Provider;
 
-class Race extends Thread implements Runnable
-{
+class Race extends Thread implements Runnable {
     boolean race = true;
-    public void run()
-    {
-        try
-        {
-         while(race){
-             System.out.println("yo");
-         }
-        }
-        catch (Exception e)
-        {
-            System.out.println ("Exception is caught");
+    boolean pause = false;
+    int x = 1;
+    public void run() {
+        try {
+            while (race) {
+                System.out.println(x);
+                x++;
+            }
+        } catch (Exception e) {
+            System.out.println("Exception is caught");
         }
     }
 }
 
 public class Controller {
-        Race race = new Race();
+    Race race = new Race();
 
     public javafx.scene.control.Button start;
     public javafx.scene.control.Button speedUp;
     public javafx.scene.control.Button slowDown;
     public javafx.scene.control.Button restart;
+    public javafx.scene.control.Button pause;
+    public javafx.scene.control.Button resume;
 
     public void startRace() {
         race = new Race();
@@ -40,6 +41,25 @@ public class Controller {
 
     public void slowDown() {
         System.out.println("slow");
+    }
+
+    public void pause() {
+        System.out.println("pause");
+        try{
+            race.wait();
+        } catch (Exception e){
+            System.out.println("lol thread broke on pause");
+        }
+    }
+
+    public void resume() {
+        System.out.println("resume");
+        try{
+            race.notify();
+        }
+        catch (Exception e){
+            System.out.println("Lol thread broke on resume");
+        }
     }
 
     public void restartRace() {
