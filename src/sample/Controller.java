6a0af2639b6fcd709b0b5ec9ba38f5fb82918592
Hiep Do <gao619@gmail.com>
2020-10-21 00:51:08 -0400

    more shapes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,21 +11,21 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
-class Race extends Thread implements Runnable {
-    boolean race = true;
-    boolean pause = false;
-    int x = 1;
-    public void run() {
-        try {
-            while (race) {
-                System.out.println(x);
-                x++;
-            }
-        } catch (Exception e) {
-            System.out.println("Exception is caught");
-        }
-    }
-}
+//class Race extends Thread implements Runnable {
+//    boolean race = true;
+//    boolean pause = false;
+//    int x = 1;
+//    public void run() {
+//        try {
+//            while (race) {
+//                System.out.println(x);
+//                x++;
+//            }
+//        } catch (Exception e) {
+//            System.out.println("Exception is caught");
+//        }
+//    }
+//}
 
 
 
@@ -33,8 +33,25 @@ class Race extends Thread implements Runnable {
 
 public class Controller {
     @FXML
-    private Rectangle rec = new Rectangle();
+    private Rectangle rec;
+
+    @FXML
+    private Rectangle rec1;
+
+    @FXML
+    private Rectangle rec2;
+
+    @FXML
+    private Rectangle rec3;
+
+    @FXML
+    private Rectangle rec4;
+
     TranslateTransition tt;
+    TranslateTransition tt1;
+    TranslateTransition tt2;
+    TranslateTransition tt3;
+    TranslateTransition tt4;
 
 
 
@@ -60,11 +77,23 @@ public class Controller {
     void restartRace(ActionEvent event) {
         tt.setRate(1.0);
         tt.playFromStart();
+        tt1.setRate(1.0);
+        tt1.playFromStart();
+        tt2.setRate(1.0);
+        tt2.playFromStart();
+        tt3.setRate(1.0);
+        tt3.playFromStart();
+        tt4.setRate(1.0);
+        tt4.playFromStart();
     }
 
     @FXML
     void slowDown(ActionEvent event) {
         tt.setRate(tt.getCurrentRate() - 1.0);
+        tt1.setRate(tt1.getCurrentRate() - 1.0);
+        tt2.setRate(tt2.getCurrentRate() - 1.0);
+        tt3.setRate(tt3.getCurrentRate() - 1.0);
+        tt4.setRate(tt4.getCurrentRate() - 1.0);
     }
 
 
@@ -75,21 +104,50 @@ public class Controller {
         tt.setFromX(0f);
         tt.setToX(900f);
         tt.play();
+        tt1 = new TranslateTransition(Duration.seconds(10), rec1);
+        tt1.setFromX(0f);
+        tt1.setToX(900f);
+        tt1.play();
+        tt2 = new TranslateTransition(Duration.seconds(15), rec2);
+        tt2.setFromX(0f);
+        tt2.setToX(900f);
+        tt2.play();
+        tt3 = new TranslateTransition(Duration.seconds(12), rec3);
+        tt3.setFromX(0f);
+        tt3.setToX(900f);
+        tt3.play();
+        tt4 = new TranslateTransition(Duration.seconds(17), rec4);
+        tt4.setFromX(0f);
+        tt4.setToX(900f);
+        tt4.play();
     }
 
     @FXML
     void speedUp(ActionEvent event) {
 
         tt.setRate(tt.getCurrentRate() + 1.0);
+        tt1.setRate(tt1.getCurrentRate() + 1.0);
+        tt2.setRate(tt2.getCurrentRate() + 1.0);
+        tt3.setRate(tt3.getCurrentRate() + 1.0);
+        tt4.setRate(tt4.getCurrentRate() + 1.0);
+
     }
 
     @FXML
     void pause(ActionEvent actionEvent) {
         tt.pause();
+        tt1.pause();
+        tt2.pause();
+        tt3.pause();
+        tt4.pause();
     }
 
     @FXML
     void resume(ActionEvent actionEvent) {
         tt.play();
+        tt1.play();
+        tt2.play();
+        tt3.play();
+        tt4.play();
     }
 }
